30e96590d197de5a558d7d115a1f8e34cab13b08
Affendrucker <devAffe@gmail.com>
2024-07-15 04:15:58 +0200

    Add sessionStorage to keep track of score & little changes
    Add a counter that wont reset on pageload to track the score of how fast you get the states
    Add coloring to the states to reflect the average score of the last five tries
    Refactor all double quotes to single ones because double quotes mess with jsonObjs
    Add resetBtn to reset the sessionStorage

--- p5.js/v2/main.js
+++ p5.js/v2/main.js
@@ -1,29 +1,44 @@
 //REFERENCES
-const stateMap = document.getElementById("state-map");
-const stateNameWhileHovering = document.getElementById("state-name-while-hovering");
-const stateNamePrompt = document.getElementById("state-name-prompt");
+const stateMap = document.getElementById('state-map');
+const stateNameWhileHovering = document.getElementById('state-name-while-hovering');
+const stateNamePrompt = document.getElementById('state-name-prompt');
+let currentMapId;
 
 //COLORS
-const startStateColor = "#126207";
-const goalStateColor = "#A3260B";
-const stateColor = "#88a4bc";
+const startStateColor = '#126207';
+const goalStateColor = '#A3260B';
+const stateColor = '#88a4bc';
+const stateColor_t1 = '#03AA00'; //darkish green
+const stateColor_t2 = '#A3D123'; //darkish yellow
+const stateColor_t3 = '#D36300'; //orange
+const stateColor_t4 = '#CE1301'; //darkish red
 // const goalStateColor_selected = ;
 // const startStateColor_selected = ;
-const stateColor_selected = "#3b729f";
-const stateColod_found = "#173669";
+const stateColor_selected = '#3b729f';
+const stateColod_found = '#173669';
 
+//CONSTANTS
+const TRIES_STORAGE = 'tries';
 //VARIABLES
+let TRIES_STORAGE_currentMap;
 let country = new Country();
 let state;
 let possibleStartStates = [];
 let possibleGoalStates = [];
 let startState;
 let goalState;
+let promptState;
+
+let triesTillFoundCorrectState = {};
+let tryCounter;
 
 stateMap.onload = function () {
   const svgDocument = stateMap.contentDocument;
   if (svgDocument) {
-    statesSVG = svgDocument.querySelectorAll(".sm_state");
+    statesSVG = svgDocument.querySelectorAll('.sm_state');
+    currentMapId = svgDocument.firstElementChild.id;
+    TRIES_STORAGE_currentMap = `${TRIES_STORAGE}_${currentMapId}`;
+
     statesSVG.forEach((stateHTML) => {
       state = new State(stateHTML);
 
@@ -33,6 +48,20 @@ stateMap.onload = function () {
       country.addState(state);
 
       //SETUP EACH STATE
+      let triesForCurrentState = JSON.parse(sessionStorage.getItem(TRIES_STORAGE_currentMap) ?? '{}')[state.id] ?? [0];
+
+      let averageTries = triesForCurrentState.reduce((a, b) => a + b) / triesForCurrentState.length;
+
+      let appliedStateColor;
+      if (averageTries < 1) appliedStateColor = stateColor;
+      else if (averageTries == 1) appliedStateColor = stateColor_t1;
+      else if (averageTries <= 2) appliedStateColor = stateColor_t2;
+      else if (averageTries <= 3) appliedStateColor = stateColor_t3;
+      else if (averageTries > 3) appliedStateColor = stateColor_t4;
+
+      // let appliedStateColor = stateColor_t3;
+      stateHTML.setAttribute('fill', appliedStateColor);
+
       // stateHTML.addEventListener("mouseover", function () {
       //   this.setAttribute("fill", stateColor_selected);
       //   stateNameWhileHovering.innerText = this.attributes.name.value;
@@ -44,9 +73,11 @@ stateMap.onload = function () {
       //   stateNameWhileHovering.style.display = "none";
       // });
 
-      stateHTML.addEventListener("click", function () {
-        if (this.id == startState.id) {
-          foundCorrectState(this, startState);
+      stateHTML.addEventListener('click', function () {
+        tryCounter++;
+
+        if (this.id == promptState.id) {
+          foundCorrectState(this, promptState);
         }
       });
     });
@@ -55,7 +86,7 @@ stateMap.onload = function () {
       state.populateNeighbors(country);
     });
   } else {
-    console.log("no svgDocument"); //---------------------------------------------------
+    console.log('no svgDocument'); //---------------------------------------------------
   }
 };
 
@@ -63,28 +94,60 @@ setTimeout(function () {
   possibleStartStates = [...country.states];
   possibleGoalStates = [...country.states];
 
-  startState = getRandomStateFrom(possibleStartStates);
-  stateNamePrompt.innerText = startState.name;
-}, 100);
+  setNewPromptState();
+}, 1000);
+
+//FUNCTIONS
+function addTryCounterToStorage(promptState, tryCounter) {
+  console.log('storing tries');
+
+  if (sessionStorage.getItem(TRIES_STORAGE_currentMap) == null) {
+    sessionStorage.setItem(TRIES_STORAGE_currentMap, '{}');
+  }
+
+  let tries = JSON.parse(sessionStorage.getItem(TRIES_STORAGE_currentMap));
+
+  let lastTenTries = tries[promptState.id] ?? [];
+
+  lastTenTries.push(tryCounter);
+  if (lastTenTries.length > 5) {
+    lastTenTries.shift();
+  }
+  tries[promptState.id] = lastTenTries;
+
+  sessionStorage.setItem(TRIES_STORAGE_currentMap, JSON.stringify(tries));
+}
 
 function foundCorrectState(stateHTML, state) {
-  stateHTML.setAttribute("fill", stateColod_found);
+  addTryCounterToStorage(state, tryCounter);
+
+  stateHTML.setAttribute('fill', stateColod_found);
 
   removeValue(state, possibleStartStates);
 
   if (possibleStartStates.length > 0) {
-    startState = getRandomStateFrom(possibleStartStates);
-    stateNamePrompt.innerText = startState.name;
+    setNewPromptState();
   } else {
     allStatesFound();
   }
 }
 
 function allStatesFound() {
-  stateNamePrompt.innerText = "all states found";
+  stateNamePrompt.innerText = 'all states found';
   window.location.reload();
 }
 
+function setNewPromptState() {
+  promptState = getRandomStateFrom(possibleStartStates);
+  stateNamePrompt.innerText = promptState.name;
+
+  tryCounter = 0;
+}
 function getRandomStateFrom(states) {
   return states[rng(states.length)];
+}
+
+function resetScore() {
+  sessionStorage.clear();
+  window.location.reload();
 }
